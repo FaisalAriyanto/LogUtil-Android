ab6e4947a2e5fe51b9e94ad59fe96adbb7f3d77b
FaisalA <vaisal.ariyanto@gmail.com>
2019-08-20 11:20:47 +0700

    create LogDebug2 (java code)

---
+++ logutil/src/main/java/pkp/map/logutil/LogDebug2.java
@@ -0,0 +1,12 @@
+package pkp.map.logutil;
+
+import android.util.Log;
+
+public class LogDebug2 {
+
+    private static final String TAG = "HUHUHU";
+
+    public static void d(String message) {
+        Log.d("", "");
+    }
+}
